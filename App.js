dadeecfdd2a49c3d335d2abe45f9b650c18f4695
priyankara97 <profront0tohero@gmail.com>
2023-09-09 15:27:31 -0700

    Reactnav

---
+++ App.js
@@ -0,0 +1,27 @@
+import 'react-native-gesture-handler';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './Pages/Login';
+import Home from './Pages/Home';
+import About from './Pages/About';
+
+
+
+const Stack = createStackNavigator();
+
+
+const App = () => {
+  return (
+    <NavigationContainer> 
+        <Stack.Navigator>
+    <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="Home" component={Home} />
+    <Stack.Screen name="About" component={About} />
+  </Stack.Navigator>
+        </NavigationContainer>
+  )
+}
+
+export default App
---
+++ Pages/Login.js
@@ -0,0 +1,13 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <View>
+      <Text>Login</Text>
+      <Button title='Login'></Button>
+    </View>
+  )
+}
+
+export default Login
